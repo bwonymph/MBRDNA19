9e95bfc9dbe7f57b46f890228a270fdf9d68e964
Varun Ved <varunmved@gmail.com>
2015-06-13 15:39:19 -0700

    added firebase components

---
+++ firebase/firebasePut.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import urllib3
+import urllib
+from firebase import firebase
+requests.packages.urllib3.disable_warnings()
+
+firebase = firebase.FirebaseApplication('https://benkpak.firebaseio.com/', None)
+
+#posts data to the cloud
+def postData():
+    url = "http://172.31.99.3/vehicle"
+    response = urllib.urlopen(url);
+    data = json.loads(response.read()) # Get data from terminal
+    #print(data)
+    localGPSlat = data['GPS_Latitude']
+    localGPSlong = data['GPS_Longitude']
+
+    firebase.put('/GPS_Latitude', "Lat", localGPSlat) # Add data to Node Node1
+    firebase.put('/GPS_Longitude', "Long", localGPSlong) # Add data to Node Node1
+
+postData();
